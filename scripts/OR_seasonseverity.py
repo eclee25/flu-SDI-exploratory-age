1386d2ce91b194137d931d705da8a6509d2c8336
Elizabeth Lee <eclee25@gmail.com>
2013-08-05 16:23:27 -0400

    plot OR vs attack rate in acute care facilities
    attack rate in acute care and inpatient facilities is a proxy for season severity
    hypothesis: as severity increases, OR decreases

---
+++ scripts/OR_seasonseverity.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+##############################################
+###Python template
+###Author: Elizabeth Lee
+###Date: 7/25/13
+###Function: Draw OR by season severity plots where season severity is defined as attack rate per 100,000 in acute care or inpatient facilities in flu peak weeks
+
+###Import data: SQL_export/OR_swk6.csv, SQL_export/seasonseverity.csv
+
+###Command Line: python 
+##############################################
+
+
+### notes ###
+
+
+### packages ###
+import matplotlib
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+## local packages ##
+import ORgenerator_v070813 as od
+
+### data structures ###
+s6dict, ORdict = {},{}
+sevdict = {} # season-severity proxy dictionary
+
+### parameters ###
+seasons = range(1,11) #seasons for which ORs will be generated
+
+### functions ###
+
+
+
+### import data ###
+s6in=open('/home/elee/Dropbox/Elizabeth_Bansal_Lab/SDI_Data/explore/SQL_export/OR_swk6.csv','r')
+s6=csv.reader(s6in, delimiter=',')
+sevin=open('/home/elee/Dropbox/Elizabeth_Bansal_Lab/SDI_Data/explore/SQL_export/seasonseverity.csv','r')
+sev=csv.reader(sevin, delimiter=',')
+
+### program ###
+od.import_d(s6, s6dict, 0, 1, 2)
+od.ORgen_seas(s6dict, ORdict, seasons)
+od.import_gen_d(sev, sevdict, 0, 3)
+
+# plot data
+xvals = [float(sevdict[k]) for k in sorted(sevdict)]
+yvals = [ORdict[k] for k in sorted(ORdict)]
+
+print "sevdict pairs", sorted(sevdict.items())
+print "sevdict keys", sorted(sevdict)
+print "sevdict values", xvals
+print "ORdict pairs", sorted(ORdict.items())
+print "ORdict keys", sorted(ORdict)
+print "ORdict values", yvals
+
+plt.scatter(xvals, yvals, marker='o', color = 'black', label= "total")
+for num, AR, OR in zip(sorted(sevdict), xvals, yvals):
+	plt.annotate(num, xy = (AR, OR), xytext = (5,0), textcoords = 'offset points')
+plt.xlabel('Attack Rate per 100,000 in acute care/inpatient at peak weeks')
+plt.ylabel('Attack Rate OR, c:a (US pop normalized, peak +/- 6wks)')
+plt.show()
+
+
+
+
+
+
+
+
+
+
