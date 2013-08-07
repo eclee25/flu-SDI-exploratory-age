af4185e3aaa8e216beb7fecdb62bd2b3a88edc26
Elizabeth Lee <eclee25@gmail.com>
2013-08-07 12:11:40 -0400

    corrected prominent subtype in subtype table
    Discrepancies were found between prominent subtypes reported in CDC Flu Summaries and a report discussing the estimated mortality associated with seasonal flu from 1976-2007. The updated subtype and subtype columns represent the prominent flu subtypes as based on number of isolates reported at each end of flu season summary. Prominent subtypes are defined as at least 20% of typed isolates in a season.

--- scripts/admin/create_subtype.sql
+++ scripts/admin/create_subtype.sql
@@ -5,8 +5,9 @@ Function: Create table subtype that lists the prominent flu subtypes for each se
 
 Command Line: mysql -u elizabeth -pbansa11ab <create_subtype.sql
 
-Data: 2000-2007 seasons from source http://www.cdc.gov/mmwr/preview/mmwrhtml/mm5933a1.htm
+Data: 2000-2007 seasons from source http://www.cdc.gov/mmwr/preview/mmwrhtml/mm5933a1.htm; There are differences between the prominent subtypes identified in this document and the percentage of H1, H3, and B isolates that are found in the Flu Season Summaries. 8/7/13 ECL
 CDC Flu Season Summary 2007-08, CDC Flu Season Summary 2008-09, CDC Flu Season Summary 2009-10 from source http://www.cdc.gov/flu/weekly/pastreports.htm
+See USfluvaxdata_June13.ods spreadsheet
 
 Codebook:
 SUBTYPE: prominent subtype(s) for that season
@@ -14,6 +15,7 @@ SUBTYPE_marker: 1 = H1; 2 = H3; 3 = B; 4 = H1 & H3; 5 = H1 & B; 6 = H3 & B; 7 = 
 (H1, H3, B, TOT)_ISOLATES: Number of isolates collected that season
 (H1, H3, B, TOT)_MATCH Number of isolates collected that season that match the vaccine strains (H1, H3, B, trivalent vax in general)
 
+8/7/13: reimported subtype table into database with corrected prominent subtypes
 */
 
 
@@ -38,13 +40,13 @@ CREATE TABLE subtype (
 INSERT INTO subtype
 VALUES 
 (1,'00-01','H1 & B',5, 354, 23, 301, 678, 354, 23, 301, 678),
-(2,'01-02','H3',2, 30, 393, 267, 690, 30, 393, 61, 484),
-(3,'02-03','H1 & B',5, 287, 143, 269, 699, 287, 143, 268, 698),
+(2,'01-02','H3 & B',6, 30, 393, 267, 690, 30, 393, 61, 484),
+(3,'02-03','H1 & H3 & B',7, 287, 143, 269, 699, 287, 143, 268, 698),
 (4,'03-04','H3',2, 3, 949, 71, 1024, 3, 106, 5, 114),
 (5,'04-05','H3 & B',6, 11, 709, 355, 1075, 11, 156, 264, 431),
-(6,'05-06','H3',2, 135, 563, 321, 1019, 135, 563, 60, 758),
+(6,'05-06','H3 & B',6, 135, 563, 321, 1019, 135, 563, 60, 758),
 (7,'06-07','H1 & H3 & B',7, 486, 289, 332, 1107, 486, 289, 254, 1029),
-(8,'07-08','H3 & B',6, 407, 404, 350, 1161, 270, 91, 6, 367),
+(8,'07-08','H1 & H3 & B',7, 407, 404, 350, 1161, 270, 91, 6, 367),
 (9,'08-09','H1 & B',5, 723, 107, 307, 1137, 723, 107, 51, 881),
 (10,'09-10','H1',1, 1906, 14, 43, 1963, 1897, 14, 38, 1949)
 ;
