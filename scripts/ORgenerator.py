cc574a2c7e7fdea7d57ef6767f3d03af4ca92b82
Elizabeth Lee <eclee25@gmail.com>
2013-09-04 20:45:49 -0400

    return vals from fxns, lat/long fxns
    ORgenerator.py
    - change functions return dictionaries and lists
    - add import_z3_ll to generate dict with z3 = (lat, long)
    - add import_z3_OR_season to generate dict with
    (seasonnum, zip3) = OR

--- scripts/ORgenerator.py
+++ scripts/ORgenerator.py
@@ -38,19 +38,25 @@ USpop = 308745538 # US total popn from 2010 Census
 
 ### functions ###
 
-#### (import_d function) import ILI data into a dictionary where season number and agegroup are the key and ILI count is the value
-# dict_data = store imported data, seascol = column number for season; agecol = column number for age group marker ('A', C', 'O'); ilicol = column number for ILI counts
+##############################################
+#### (import_d function) dict_data = store imported data, seascol = column number for season; agecol = column number for age group marker ('A', C', 'O'); ilicol = column number for ILI counts
 # length of dictionary should equal number of rows in csvfile
 def import_d (csvfile, seascol, agecol, ilicol):
+	''' Import season-level ILI data into a dictionary where season number and agegroup are the key and ILI count is the value.
+	'''
+
 	dict_data = {}
 	for row in csvfile:
 		dict_data[(int(row[seascol]), str(row[agecol]))] = float(row[ilicol])
 	print "Length of dict_data: %d" % len(dict_data)
 	return dict_data
 
-#### (ORgen_seas function) generate odds ratios at the season-level for a given list of attack rates for children and adults
-# dict_data = completed dictionary with (season number, age group) as key and ILI count as value; dict_OR = store ORs; timelist = list of season numbers for which ORs will be calculated
+##############################################
+#### (ORgen_seas function) dict_data = completed dictionary with (season number, age group) as key and ILI count as value; dict_OR = store ORs; timelist = list of season numbers for which ORs will be calculated
 def ORgen_seas (dict_data, timelist):
+	''' Generate a dictionary of season-level odds ratios for a given list of attack rates for children and adults, where the key is the season number and the value is the odds ratio.
+	'''
+
 	dict_OR = {}
 	for t in timelist:
 		c_attack = dict_data[(t,'C')]/USchild # calculate child attack rate
@@ -61,8 +67,12 @@ def ORgen_seas (dict_data, timelist):
 	print "Length of dict_OR: %d" % len(dict_OR)
 	return dict_OR
 
-#### (ARdict_seas function) generate dictionary for season number and attack rates of children and adults normalized by the entire US population
+##############################################
+#### (ARdict_seas function) 
 def ARdict_seas (dict_data, timelist):
+	''' Generate dictionaries of season-level attack rates for the total US population, US children, and US adults. The key is the season number for both dicts. For one dict, the value is the total attack rate. For the second dict, the value is a tuple of the child and adult attack rates.
+	'''
+
 	dict_AR_ca, dict_AR_tot = {}, {}
 	for t in timelist:
 		# child attack rate
@@ -75,11 +85,13 @@ def ARdict_seas (dict_data, timelist):
 		dict_AR_tot[t] = tot_attack
 	return dict_AR_ca, dict_AR_tot
 
-
-#### (import_dwk function) import ILI data into a dictionary where season number and agegroup are the key and ILI count is the value
-# dict_data = store imported data; dict_wk = empty dictionary where week and season numbers are keys and  seascol = column number for season; wkcol = column number for week; agecol = column number for age group marker ('A', C', 'O'); ilicol = column number for ILI counts; wklist = list where unique weeks will be appended
+##############################################
+#### (import_dwk function) dict_data = store imported data; dict_wk = empty dictionary where week and season numbers are keys and  seascol = column number for season; wkcol = column number for week; agecol = column number for age group marker ('A', C', 'O'); ilicol = column number for ILI counts; wklist = list where unique weeks will be appended
 # length of dictionary should equal number of rows in csvfile
 def import_dwk (csvfile, seascol, wkcol, agecol, ilicol):
+	''' Import ILI data into a dictionary where a tuple of week and age group are the keys and the values are the number of ILI cases. Create a second dictionary where week is the key and associated season number is the value. In addition, return a unique list of weeks in the file, which will be used to call the values in these two dictionaries in the main code.
+	'''
+
 	dict_data, dict_wk, wklist = {}, {}, []
 	for row in csvfile:
 		week = row[wkcol]
@@ -91,10 +103,12 @@ def import_dwk (csvfile, seascol, wkcol, agecol, ilicol):
 	wklist = set(wklist)
 	return dict_data, dict_wk, wklist
 
-
-#### (ORgen_wk function) generate odds ratios at the season-level for a given list of attack rates for children and adults
-# dict_data = completed dictionary with (week, age group) as key and ILI count as value; dict_OR = store ORs in dict; timelist = list of season numbers for which ORs will be calculated; 
+##############################################
+#### (ORgen_wk function) dict_data = completed dictionary with (week, age group) as key and ILI count as value; dict_OR = store ORs in dict; timelist = list of season numbers for which ORs will be calculated; 
 def ORgen_wk (dict_data, wklist):
+	''' Generate odds ratios at the week-level. Create one dictionary where key is the week and value is the OR. Create a second dictionary where key is the week and value is the total attack rate by 10,000 for the week. 
+	'''
+	
 	dict_OR, dict_AR = {},{}
 	for w in set(wklist):
 		c_attack = dict_data[(w, 'C')]/USchild # calculate child attack rate
@@ -106,30 +120,60 @@ def ORgen_wk (dict_data, wklist):
 	print "Length of dict_OR: %d" % len(dict_OR)
 	return dict_OR, dict_AR
 
-
-#### (import_gen_d function) generically import data into a dictionary
-# kcol = column for keys; vcol = column for dicitonary values; dict_gen = empty dictionary where data will be stored
-def import_gen_d (csvfile, dict_gen, kcol, vcol):
+##############################################
+#### (import_gen_d function) kcol = column for keys; vcol = column for dicitonary values; dict_gen = empty dictionary where data will be stored
+def import_gen_d (csvfile, kcol, vcol):
+	''' Generic function for importing data into a dictionary. 
+	'''
+	
+	dict_gen = {}
 	for row in csvfile:
 		dict_gen[int(row[kcol])] = str(row[vcol])
 	print "Length of new dict: %d" % len(dict_gen)
+	return dict_gen
 
-
-
-#### (import_cdc function) import ILI data into a dictionary where season number and agegroup are the key and ILI count is the value
-# dict_data = empty dictionary where imported data will be stored, wkcol = column number for wknum, childcol = column number for child ILI cases, adultcol = column number for adult ILI cases, wklist = empty list where unique weeks will be appended
+##############################################
+#### (import_cdc function) dict_data = empty dictionary where imported data will be stored, wkcol = column number for wknum, childcol = column number for child ILI cases, adultcol = column number for adult ILI cases, wklist = empty list where unique weeks will be appended
 # length of dictionary should equal number of rows in csvfile
-def import_cdc (csvfile, dict_data, wkcol, childcol, adultcol, wklist):
+def import_cdc (csvfile, wkcol, childcol, adultcol):
+	''' Import ILI data into a dictionary where the key is a tuple of the week number and age group and the value is the ILI count. This function was used to import CDC data, where the definitions of children and adults do not exactly match the definitions we are using.
+	'''
+	
+	dict_data, wklist = {}, []
 	for row in csvfile:
 		wknum = row[wkcol]
 		wklist.append(wknum)
 		dict_data[(wknum, 'C')] = float(row[childcol]) # cdc data is 5-24 for children
 		dict_data[(wknum, 'A')] = float(row[adultcol]) # cdc data is 25-64 for adults
 	print "Length of dict_data: %d" % len(dict_data)
+	return dict_data, wklist
 
+##############################################
+def import_z3_ll(fname):
+	''' Import dataset with zip3, latitude, and longitude into a dictionary where the key is the zip3 and the value is a tuple of the latitude and longitude.'''
+	
+	dict_z3_ll = {}
+	lines = fname.readlines()
+	fname.close()
+	for line in lines:
+		fields = line.split(',')
+		# dict_z3_ll[zip3] = (latitude, longitude)
+		dict_z3_ll[fields[0]] = (float(fields[1]), float(fields[2]))
+	return dict_z3_ll
 
-
-
+##############################################
+def import_z3_OR_season(fname):
+	''' Import dataset with season, zip3, and odds ratio into a dictionary where the key is a tuple of the season and zip3 and the value is the OR.
+	'''
+	
+	dict_z3_OR_season = {}
+	lines = fname.readlines()
+	fname.close()
+	for line in lines:
+		fields = line.split(',')
+		# dict_z3_OR_season[(season, zip3)] = OR
+		dict_z3_OR_season[(int(fields[0]), fields[1])] = float(fields[2])
+	return dict_z3_OR_season
 
 
 
