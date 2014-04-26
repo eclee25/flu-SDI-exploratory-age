d907bd58c46a04db2c1329d6705759a586ebedf6
Elizabeth Lee <eclee25@gmail.com>
2014-04-26 13:56:43 -0400

    fc: F3 zOR vs. time
    F3_zOR_time.py
    - zOR with normweeks length baseline (outpatient
    data only)
    - no grey bars for classification because the metric
    weeks are relative to the peak and Thanksgiving weeks
    for the retrospective and early warning periods,
    respectively

---
+++ scripts/create_fluseverity_figs/F3_zOR_time.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+##############################################
+###Python template
+###Author: Elizabeth Lee
+###Date: 4/26/14
+###Function: OR of incidence in children to incidence in adults vs. week number normalized by the first 'gp_normweeks' of the season. Incidence in children and adults is normalized by the size of the child and adult populations in the second calendar year of the flu season.
+
+###Import data: SQL_export/OR_allweeks_outpatient.csv, SQL_export/totalpop.csv, My_Bansal_Lab/Clean_Data_for_Import/ThanksgivingWeekData_cl.csv
+
+###Command Line: python F3_zOR_time.py
+##############################################
+
+### notes ###
+# Incidence per 100,000 is normalized by total population by second calendar year of the flu season
+
+### packages/modules ###
+import csv
+import matplotlib.pyplot as plt
+
+## local modules ##
+import functions as fxn
+
+### data structures ###
+### functions ###
+### data files ###
+incidin = open('/home/elee/Dropbox/Elizabeth_Bansal_Lab/SDI_Data/explore/SQL_export/OR_allweeks_outpatient.csv','r')
+incid = csv.reader(incidin, delimiter=',')
+popin = open('/home/elee/Dropbox/Elizabeth_Bansal_Lab/SDI_Data/explore/SQL_export/totalpop_age.csv', 'r')
+pop = csv.reader(popin, delimiter=',')
+
+### called/local plotting parameters ###
+ps = fxn.gp_plotting_seasons
+fw = fxn.gp_fluweeks
+sl = fxn.gp_seasonlabels
+colvec = fxn.gp_colors
+wklab = fxn.gp_weeklabels
+norm = fxn.gp_normweeks
+fs = 24
+fssml = 16
+
+### program ###
+# import data
+# d_wk[week] = seasonnum, d_incid53ls[seasonnum] = [ILI wk 40 per 100000, ILI wk 41 per 100000,...], d_OR53ls[seasonnum] = [OR wk 40, OR wk 41, ...], d_zOR53ls[seasonnum] = [zOR wk 40, zOR wk 41, ...]
+d_wk, d_incid53ls, d_OR53ls, d_zOR53ls = fxn.week_plotting_dicts(incid, pop)
+
+# plot values
+for s in ps:
+	plt.plot(xrange(fw), d_zOR53ls[s][:fw], marker = 'o', color = colvec[s-2], label = sl[s-2], linewidth = 2)
+# grey bars for potential early warning period?
+# grey bars for potential range of retrospective period?
+# create dict with begin_retro and begin_early for each season?
+plt.xlim([0, fw-1])
+plt.xticks(range(fw)[::5], wklab[:fw:5]) 
+plt.xlabel('Week Number', fontsize=fs)
+plt.ylabel('zOR (%s week baseline)' % (norm), fontsize=fs)
+plt.legend(loc='upper left')
+plt.savefig('/home/elee/Dropbox/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/fluseverity_figs/F3/zOR_time.png', transparent=False, bbox_inches='tight', pad_inches=0)
+plt.close()
+
+
+
+
