e2b4316dff8e8bd5c2a3bb18e39c5e07ec8e6861
Elizabeth Lee <eclee25@gmail.com>
2014-06-03 22:56:03 -0400

    changes to charts for Biggerstaff talk

--- scripts/create_fluseverity_figs/F3_zOR_time.py
+++ scripts/create_fluseverity_figs/F3_zOR_time.py
@@ -44,18 +44,35 @@ fssml = 16
 # d_wk[week] = seasonnum, d_incid53ls[seasonnum] = [ILI wk 40 per 100000, ILI wk 41 per 100000,...], d_OR53ls[seasonnum] = [OR wk 40, OR wk 41, ...], d_zOR53ls[seasonnum] = [zOR wk 40, zOR wk 41, ...]
 d_wk, d_incid53ls, d_OR53ls, d_zOR53ls = fxn.week_plotting_dicts(incid, pop)
 
+# # plot values
+# for s in ps:
+# 	plt.plot(xrange(fw), d_zOR53ls[s][:fw], marker = 'o', color = colvec[s-2], label = sl[s-2], linewidth = 2)
+# # grey bars for potential early warning period?
+# # grey bars for potential range of retrospective period?
+# # create dict with begin_retro and begin_early for each season?
+# plt.xlim([0, fw-1])
+# plt.xticks(range(fw)[::5], wklab[:fw:5]) 
+# plt.xlabel('Week Number', fontsize=fs)
+# plt.ylabel('zOR (%s week baseline)' % (norm), fontsize=fs)
+# plt.legend(loc='upper left')
+# plt.savefig('/home/elee/Dropbox/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/fluseverity_figs/F3/zOR_time.png', transparent=False, bbox_inches='tight', pad_inches=0)
+# plt.close()
+
+## BIGGERSTAFF PRESENTATION PLOT ##
 # plot values
 for s in ps:
 	plt.plot(xrange(fw), d_zOR53ls[s][:fw], marker = 'o', color = colvec[s-2], label = sl[s-2], linewidth = 2)
-# grey bars for potential early warning period?
-# grey bars for potential range of retrospective period?
 # create dict with begin_retro and begin_early for each season?
 plt.xlim([0, fw-1])
 plt.xticks(range(fw)[::5], wklab[:fw:5]) 
 plt.xlabel('Week Number', fontsize=fs)
 plt.ylabel('zOR (%s week baseline)' % (norm), fontsize=fs)
 plt.legend(loc='upper left')
-plt.savefig('/home/elee/Dropbox/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/fluseverity_figs/F3/zOR_time.png', transparent=False, bbox_inches='tight', pad_inches=0)
+# grey bar for approximate retrospective period
+plt.fill([15, 16, 16, 15], [-20, -20, 40, 40], facecolor='grey', alpha=0.4)
+# # grey bar for approximate early warning area
+# plt.fill([9, 10, 10, 9], [-20, -20, 40, 40], facecolor='grey', alpha=0.4)
+plt.savefig('/home/elee/Dropbox/Elizabeth_Bansal_Lab/Presentations/Biggerstaff_2014_6_2/Figures/zOR_time_retro.png', transparent=False, bbox_inches='tight', pad_inches=0)
 plt.close()
 
 
