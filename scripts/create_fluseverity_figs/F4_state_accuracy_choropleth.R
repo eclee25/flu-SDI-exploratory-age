0c1cb4f6c4300af9c7d4d6ef83484d3c886a5ae9
Elizabeth Lee <eclee25@gmail.com>
2014-07-29 18:46:27 -0400

    add fig titles and adjust formatting
    F4_state_accuracy_choropleth.R
    - add figure titles for manuscript choropleths
    - decreased title fontsize for readability

--- scripts/create_fluseverity_figs/F4_state_accuracy_choropleth.R
+++ scripts/create_fluseverity_figs/F4_state_accuracy_choropleth.R
@@ -79,29 +79,32 @@ choropleth2 <- choropleth2[order(choropleth2$order),]
 map_est_rnat <- ggplot(choropleth2, aes(long, lat, group=group)) +
   geom_polygon(aes(fill=earlyst_retronat), size = 0.2) +
   geom_polygon(data=us_state_map, color='white', fill=NA) +
-  theme_minimal(base_size = 16, base_family = "") +
+  theme_minimal(base_size = 9, base_family = "") +
   theme(panel.background = element_blank(),panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.ticks = element_blank(), axis.text.y = element_blank(), axis.text.x = element_blank()) +
     labs(x=NULL, y=NULL) +
-  scale_fill_brewer("# matches", type='seq', palette=2, labels=levels(orig3$earlyst_retronat), drop=FALSE)
+  scale_fill_brewer("# matches", type='seq', palette=2, labels=levels(orig3$earlyst_retronat), drop=FALSE)  + 
+  ggtitle('early warning (state) and retrospective (national)')
 ggsave(map_est_rnat, width=5, height=3, file='earlySt_retroNat_matchCt.png')
 
 # retrospective state match retrospective national
 map_rst_rnat <- ggplot(choropleth2, aes(long, lat, group=group)) +
   geom_polygon(aes(fill=retrost_retronat), size = 0.2) +
   geom_polygon(data=us_state_map, color='white', fill=NA) +
-  theme_minimal(base_size = 16, base_family = "") +
+  theme_minimal(base_size = 9, base_family = "") +
   theme(panel.background = element_blank(),panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.ticks = element_blank(), axis.text.y = element_blank(), axis.text.x = element_blank()) +
   labs(x=NULL, y=NULL) +
-  scale_fill_brewer("# matches", type='seq', palette=11, labels=levels(orig3$retrost_retronat), drop=FALSE)
+  scale_fill_brewer("# matches", type='seq', palette=11, labels=levels(orig3$retrost_retronat), drop=FALSE) + 
+  ggtitle('retrospective (state) and retrospective (national)')
 ggsave(map_rst_rnat, width=5, height=3, file='retroSt_retroNat_matchCt.png')
 
 # early warning state match retrospective state
 map_est_rst <- ggplot(choropleth2, aes(long, lat, group=group)) +
   geom_polygon(aes(fill=earlyst_retrost), size = 0.2) +
   geom_polygon(data=us_state_map, color='white', fill=NA) +
-  theme_minimal(base_size = 16, base_family = "") +
+  theme_minimal(base_size = 9, base_family = "") +
   theme(panel.background = element_blank(),panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.ticks = element_blank(), axis.text.y = element_blank(), axis.text.x = element_blank()) +
   labs(x=NULL, y=NULL) +
-  scale_fill_brewer("# matches", type='seq', palette=3, labels=levels(orig3$earlyst_retrost), drop=FALSE)
+  scale_fill_brewer("# matches", type='seq', palette=3, labels=levels(orig3$earlyst_retrost), drop=FALSE) + 
+  ggtitle('early warning (state) and retrospective (state)')
 ggsave(map_est_rst, width=5, height=3, file='earlySt_retroSt_matchCt.png')
 
