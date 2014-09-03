8d9d0c5c5d48c2f3e204ba32a62024d4e2fb66a1
Elizabeth Lee <eclee25@gmail.com>
2014-09-03 09:05:26 -0400

    benchmark bars, 01-02 to 08-09 orig vs excesspi
    F3_benchmarkbars.R
    - add bar plot with original benchmark values for
    01-02 to 08-09 season
    - add bar plot where proportion of all-cause mortality
    due to P&I has been replaced with number of excess
    P&I deaths (as calculated from weekly seasonal baseline
    data provided by Matt for 01-02 to 08-09 seasons)
    - these two bar plots are for benchmark composition
    supplemental section

--- scripts/create_fluseverity_figs/F3_benchmarkbars.R
+++ scripts/create_fluseverity_figs/F3_benchmarkbars.R
@@ -39,11 +39,6 @@ dfsumm<-function(x) {
 	print(s)
 } 
 
-
-# read data
-setwd('/home/elee/Dropbox/Elizabeth_Bansal_Lab/SDI_Data/explore/R_export')
-nat <-read.csv('regression_data.csv', header=T, colClasses='numeric')
-
 # plot formatting
 w = 464 
 h = 464
@@ -51,6 +46,15 @@ ps = 14
 margin = c(1, 4, 1, 4) + 0.1 # bottom, left, top, right
 omargin = c(1, 1, 1, 1)
 un = "px"
+
+###################################
+## 8/16/14 plot benchmark index long ##
+
+# read data
+setwd('/home/elee/Dropbox/Elizabeth_Bansal_Lab/SDI_Data/explore/R_export')
+nat <-read.csv('regression_data.csv', header=T, colClasses='numeric')
+
+# specific formatting
 seasonlab <- c('97-98', '98-99', '99-00', '00-01', '01-02', '02-03', '03-04', '04-05', '05-06', '06-07', '07-08', '08-09', '10-11', '11-12', '12-13', '13-14')
 # assign colors based on severity
 # red severe, yellow moderate, blue mild
@@ -60,15 +64,59 @@ colorvec <- rep('blue',16)
 colorvec[sev] <- 'red'
 colorvec[mod] <- 'yellow'
 
-###################################
-## 8/16/14 plot benchmark index ##
-
 # barplot
 setwd('/home/elee/Dropbox/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/fluseverity_figs/F3')
 par(mar = margin)
 png(filename="benchmarkbars.png", units=un, width=w, height=h, pointsize=ps, bg = 'white')
-mids <- barplot(nat$ix_noILI, xlab='', ylab='Benchmark Index', ylim=c(-6,6), col = colorvec)
+mids <- barplot(nat$ix_noILI, xlab='', ylab=expression(paste('Benchmark, ', beta, sep=' ')), ylim=c(-6,6), col = colorvec)
 axis(1, at=mids, seasonlab, las = 2)
 mtext('Season', side=1, line=3.5)
 dev.off()
 
+###################################
+## plot benchmark index 01-02 to 08-09 ## 9/3/14
+setwd('/home/elee/Dropbox/Elizabeth_Bansal_Lab/CDC_Source/Import_Data')
+og <- read.csv('cdc_severity_index.csv', header=T, colClasses='numeric')
+
+# specific formatting
+seasonlab <- c('01-02', '02-03', '03-04', '04-05', '05-06', '06-07', '07-08', '08-09')
+# assign colors based on severity
+# red severe, yellow moderate, blue mild
+sev <- which(og$ix_noILI> 1)
+mod <- which(og$ix_noILI<= 1 & og$ix_noILI >= -1)
+colorvec <- rep('blue',8)
+colorvec[sev] <- 'red'
+colorvec[mod] <- 'yellow'
+
+# barplot
+setwd('/home/elee/Dropbox/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/fluseverity_figs/Supp/bench_composition')
+par(mar = margin)
+png(filename="benchmarkbars_orig.png", units=un, width=w, height=h, pointsize=ps, bg = 'white')
+mids <- barplot(og$ix_noILI, xlab='', ylab=expression(paste('Benchmark, ', beta, sep=' ')), ylim=c(-6,6), col = colorvec)
+axis(1, at=mids, seasonlab, las = 2)
+mtext('Season', side=1, line=3.5)
+dev.off()
+
+###################################
+## plot benchmark index 01-02 to 08-09, excesspi replacement ## 9/3/14
+setwd('/home/elee/Dropbox/Elizabeth_Bansal_Lab/CDC_Source/Import_Data')
+exc <- read.csv('cdc_severity_index_excesspi.csv', header=T, colClasses='numeric')
+
+# specific formatting
+seasonlab <- c('01-02', '02-03', '03-04', '04-05', '05-06', '06-07', '07-08', '08-09')
+# assign colors based on severity
+# red severe, yellow moderate, blue mild
+sev <- which(exc$ix_noILI> 1)
+mod <- which(exc$ix_noILI<= 1 & exc$ix_noILI >= -1)
+colorvec <- rep('blue',8)
+colorvec[sev] <- 'red'
+colorvec[mod] <- 'yellow'
+
+# barplot
+setwd('/home/elee/Dropbox/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/fluseverity_figs/Supp/bench_composition')
+par(mar = margin)
+png(filename="benchmarkbars_excesspi.png", units=un, width=w, height=h, pointsize=ps, bg = 'white')
+mids <- barplot(exc$ix_noILI, xlab='', ylab=expression(paste('Benchmark, ', beta, sep=' ')), ylim=c(-6,6), col = colorvec)
+axis(1, at=mids, seasonlab, las = 2)
+mtext('Season', side=1, line=3.5)
+dev.off()
