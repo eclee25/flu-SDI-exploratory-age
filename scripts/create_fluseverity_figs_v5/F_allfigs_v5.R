bad0dc2f52017688d8d4f996a2f2494cf7db2e53
Elizabeth Lee <eclee25@gmail.com>
2015-07-27 15:39:02 -0400

    benchmark Fig1, 1997-2014 years
    F_allfigs_v5.R
    - update figure 1 with panel e - barplot with benchmark
    - extend from 1997-98 to 2013-14 season (excl. 2009-10)
    - change margin, axis label, and legend formatting

--- scripts/create_fluseverity_figs_v5/F_allfigs_v5.R
+++ scripts/create_fluseverity_figs_v5/F_allfigs_v5.R
@@ -6,12 +6,15 @@
 ## Filenames: SQL_export/F1.csv, CDC_Source/Import_Data/all_cdc_source_data.csv
 ## Data Source: 'season', 'wk', 'yr', 'wknum', 'ILI', 'anydiag', 'pop'
 ## Notes: F1.csv: 'season', 'wk', 'yr', 'wknum', 'outpatient & office ILI', 'outpatient & office anydiag', 'pop'
-## 11/2/14 Note: createF1.csv includes data from all service places while F1.csv includes only outpatient data
+## 11/2/14 Note: createF1.csv includes data from all service places while F1.csv includes only outpatient data\
+## 7/27/15: add benchmark bars to panel e, extend from 1997-2014 (excl. 2009-10), change fig labels
 ## 
 ## useful commands:
 ## install.packages("pkg", dependencies=TRUE, lib="/usr/local/lib/R/site-library") # in sudo R
 ## update.packages(lib.loc = "/usr/local/lib/R/site-library")
 
+require(dplyr)
+require(tidyr)
 # 12/4/14
 setwd('/home/elee/R/source_functions')
 source("dfsumm.R")
@@ -31,16 +34,21 @@ sdi1wknum$wknum <- paste0('0', sdi1wknum$wknum)
 sdi2wknum <- sdi[sdi$wknum > 9,]
 sdi3 <- rbind(sdi1wknum, sdi2wknum)
 sdi3$uqid <- paste0(sdi3$yr, sdi3$wknum)
-# 11/2/14 subset S2 through S9 data
-sdi2 <- sdi3[(sdi3$uqid >= 200140 & sdi3$uqid <= 200920),]
+# 7/27/15: rm subset
+# # 11/2/14 subset S2 through S9 data
+# sdi2 <- sdi3[(sdi3$uqid >= 200140 & sdi3$uqid <= 200920),]
+sdi2 <- sdi3
 # reorder sdi2 by uqid
 sdi2 <- sdi2[order(sdi2$uqid, decreasing = FALSE),]
+
 # CDC
 setwd('/home/elee/Dropbox/Elizabeth_Bansal_Lab/CDC_Source/Import_Data')
 cdc <- read.csv('all_cdc_source_data.csv', colClasses = 'character', header = T)
 cdc$uqid <- as.character(cdc$uqid)
-# 11/2/14 subset S2 through S9 data
-cdc_lm <- cdc[(cdc$uqid >= '200140' & cdc$uqid <= '200920'),]
+# 7/27/15L rm subset
+# # 11/2/14 subset S2 through S9 data
+# cdc_lm <- cdc[(cdc$uqid >= '200140' & cdc$uqid <= '200920'),]
+cdc_lm <- cdc
 cdc_lm$perc_pos <- as.numeric(cdc_lm$perc_pos)
 cdc_lm$a_H1 <- as.numeric(cdc_lm$a_H1)
 cdc_lm$a_H3sum <- as.numeric(cdc_lm$a_H3) + as.numeric(cdc_lm$a_H3N2)
@@ -56,9 +64,9 @@ cdc_lm$perc_unwt_ili <- as.numeric(cdc_lm$perc_unwt_ili)
 
 # check uqids across cdc_lm and sdi2
 cdc_lm$uqid %in% sdi2$uqid
-cdc_lm[c(118, 379),] # not in sdi2: 200353, 200853
+cdc_lm[c(327, 588),] # not in sdi2: 200353, 200853
 sdi2$uqid %in% cdc_lm$uqid
-sdi2[274,] # not in cdc_lm: 200053, 200653
+sdi2[c(1, 314),] # not in cdc_lm: 200053, 200653
 
 
 ##########################################
@@ -71,7 +79,7 @@ sdi2$ILI_diag_perc <- sdi2$ILI/sdi2$anydiag * 100
 cdc_lm$hosp_tot_diff <- NA
 cdc_lm$hosp_5.17_diff <- NA
 cdc_lm$hosp_18.49_diff <- NA
-for (s in 2:9){
+for (s in -2:14){
   dat <- cdc_lm[cdc_lm$season == s,]
   cdc_lm$hosp_tot_diff[cdc_lm$season == s] <- c(dat$hosp_tot[1], diff(dat$hosp_tot, lag = 1))
   cdc_lm$hosp_5.17_diff[cdc_lm$season == s] <- c(dat$hosp_5.17[1], diff(dat$hosp_5.17, lag = 1))
@@ -81,68 +89,109 @@ for (s in 2:9){
 ##########################################
 # 2/11/15: process pediatric deaths so that they are cumulative
 cdc_lm$ped_deaths_cum <- NA
-for (s in 2:9){
+for (s in -2:14){
   dat <- cdc_lm[cdc_lm$season == s,]
   # put NA for first week in every season so that the cumulative deaths will not be connected across seasons (there are no ped deaths in those weeks anyways)
   cdc_lm$ped_deaths_cum[cdc_lm$season == s] <- c(NA, cumsum(dat$ped_deaths[2:length(dat$ped_deaths)]))
 }
+
+##########################################
+# 7/27/15 benchmark barplot
+setwd('/home/elee/Dropbox/Elizabeth_Bansal_Lab/SDI_Data/explore/R_export')
+benchmark <- read.csv('benchmark_ixTavg_altnorm_comparisons.csv', header=T)
+
+##########################################
+# 7/27/15 merge cdc_lm and sdi2 data 
+sdi_drop <- tbl_df(sdi2) %>% select(uqid, ILI_diag_perc)
+cdc_drop <- tbl_df(cdc_lm) %>% select(uqid, yr, wk, season, perc_unwt_ili, a_H1, a_H3, b, perc_pos, ped_deaths_cum, hosp_5.17, hosp_18.49, pi_only)
+merge1 <- left_join(cdc_drop, sdi_drop, by = "uqid") 
+merge2 <- merge1 %>% filter(uqid < "200940" | uqid >= "201040")
+
 ##########################################
 # plotting parameters
 w = 580 
-h = 580
+h = 700
 ps = 14
-margin = c(1, 4, 1, 4) + 0.1 # bottom, left, top, right
-lastmargin = c(4, 4, 0, 4)
-omargin = c(1, 1, 1, 1)
+margin = c(0.5, 4, 1, 4) # bottom, left, top, right
+omargin = c(3.5, 0, 0, 0)
 sz = 2
 sz2 = 1.2
+sz3 = 2.5
 un = "px"
-wkticks = c(seq(1, dim(sdi2)[1], by = 52), dim(sdi2)[1])
-wklabs = paste(c(rep('Oct', length(wkticks)-1), 'May'), substr(sdi2$wk[wkticks], 3, 4), sep=' ')
+wkticks <- which(substring(merge2$uqid, 5, 6)=='40')
+# wklabs <- paste(c(rep('Oct', length(wkticks)-1), 'May'), substr(merge2$yr[wkticks], 3, 4), sep=' ')
+seasonlabs <- c('97-98', '98-99', '99-00', '00-01', '01-02', '02-03', '03-04', '04-05', '05-06', '06-07', '07-08', '08-09', '10-11', '11-12', '12-13', '13-14')
+# benchmark bar colors
+sev <- which(benchmark$b0_classifq==1)
+mod <- which(benchmark$b0_classifq==0)
+colorvec <- rep('blue',16)
+colorvec[sev] <- 'red'
+colorvec[mod] <- 'yellow'
 
 ##########################################
 # combined figure
-setwd('/home/elee/Dropbox/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/fluseverity_figs_v5/F1')
+setwd('/home/elee/Dropbox (Bansal Lab)/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/Submission_Materials/BMCMedicine/Submission2/MainFigures/F1')
 
 png(filename="all_panels.png", units=un, width=w, height=h, pointsize=ps, bg = 'white')
-par(mfrow = c(4, 1), mar = margin)
+par(mfrow = c(5, 1), mar = margin, oma = omargin)
 
 # ILI panel
-plot(sdi2$ILI_diag_perc, type = 'l', xlab = '', ylab = '', ylim = c(0, 7.5), col = 'tomato', axes = F, lwd = sz, cex.lab = sz)
-axis(2, at = seq(0, 7, by=1), labels=seq(0, 7, by=1))
-mtext(2, text = 'ILI / outpat. (%)', line = 2, cex = sz2)
-lines(cdc_lm$perc_unwt_ili, type = 'l', col = 'turquoise3', lwd = sz)
-legend('topleft', c('ILINet data', 'medical claims data'), lwd = sz, col = c('turquoise3', 'tomato'))
+plot(merge2$ILI_diag_perc, type = 'l', xlab = '', ylab = '', ylim = c(0, 8), col = 'tomato', axes = F, lwd = sz, cex.lab = sz)
+abline(v = which(merge2$uqid=='201040'), lwd = sz2, col = 'black')
+abline(h = 0, lwd = sz2, col = 'black')
+axis(2, at = seq(0, 7, by=2), labels=seq(0, 7, by=2))
+mtext(2, text = 'percent', line = sz3, cex = sz2)
+lines(merge2$perc_unwt_ili, type = 'l', col = 'turquoise3', lwd = sz)
+legend('topleft', c('medical claims', 'ILINet'), lwd = sz, col = c('tomato', 'turquoise3'), , title = 'ILI (%) of all visits')
 
 # perc_pos panel
 par(mar=margin)
-plot(cdc_lm$a_H1, col = 'navy', type = 'l', axes = F, ylab = '', xlab = '', ylim = c(0, 1600))
-lines(cdc_lm$a_H3sum, col = 'violetred3', type = 'l', lwd = sz2)
-lines(cdc_lm$b, col = 'orange', type = 'l', lwd = sz2)
-axis(2, at = seq(0, 1600, by=500), labels = seq(0, 1600, by=500), col.axis = 'black', col = 'black')
-mtext(2, text = 'samples (#)', line = 2.5, cex = sz2, col = 'black')
+plot(merge2$a_H1, col = 'navy', type = 'l', axes = F, ylab = '', xlab = '', ylim = c(0, 1600))
+lines(merge2$a_H3sum, col = 'violetred3', type = 'l', lwd = sz2)
+lines(merge2$b, col = 'orange', type = 'l', lwd = sz2)
+abline(v = which(merge2$uqid=='201040'), lwd = sz2, col = 'black')
+abline(h = 0, lwd = sz2, col = 'black')
+axis(2, at = seq(0, 1600, by=750), labels = seq(0, 1600, by=750), col.axis = 'black', col = 'black')
+mtext(2, text = 'samples', line = sz3, cex = sz2, col = 'black')
 par(new=T)
-plot(cdc_lm$perc_pos, type = 'l', xlab = '', ylab = '', ylim = c(0, 35), col = 'black', axes = F, lwd = sz, cex.lab = sz)
+plot(merge2$perc_pos, type = 'l', xlab = '', ylab = '', ylim = c(0, 35), col = 'black', axes = F, lwd = sz, cex.lab = sz)
 axis(4, at=seq(0, 35, by=15), labels=seq(0, 35, by=15))
-mtext(4, text = 'positive tests (%)', line = 2, cex = sz2)
-legend('topleft', c('A/H1 samp.', 'A/H3 samp.', 'B samp.', '% pos tests'), col = c('navy', 'violetred3', 'orange', 'black'), lwd = c(sz2, sz2, sz2, sz))
+mtext(4, text = 'percent', line = sz3, cex = sz2)
+legend('topleft', c('A/H1 samples', 'A/H3 ...', 'B ...', 'positive tests (%)'), col = c('navy', 'violetred3', 'orange', 'black'), lwd = c(sz2, sz2, sz2, sz))
 
 # laboratory confirmed panel (cumulative hospitalization rates, pediatric deaths)
 par(mar=margin)
-plot(cdc_lm$ped_deaths_cum, col = 'forestgreen ', type = 'l', lwd = sz, axes = F, ylab = '', xlab = '', ylim = c(0,100))
-axis(4, at = seq(0, 100, by=20), labels = seq(0, 100, by=20), col.axis = 'black', col = 'black', col.ticks = 'black')
-mtext(4, text = 'cum. deaths (#)', line = 2.5, col = 'black', cex = sz2)
+plot(merge2$ped_deaths_cum, col = 'forestgreen ', type = 'l', lwd = sz, axes = F, ylab = '', xlab = '', ylim = c(0, 200))
+abline(v = which(merge2$uqid=='201040'), lwd = sz2, col = 'black')
+abline(h = 0, lwd = sz2, col = 'black')
+axis(4, at = seq(0, 200, by=100), labels = seq(0, 200, by=100), col.axis = 'black', col = 'black', col.ticks = 'black')
+mtext(4, text = 'deaths', line = sz3, col = 'black', cex = sz2)
 par(new=T)
-plot(cdc_lm$hosp_5.17, type = 'l', xlab = '', ylab = '', axes = F, lwd = sz, ylim = c(0, 10), cex.lab = sz, col = 'red')
-lines(cdc_lm$hosp_18.49, type = 'l', col = 'blue', lwd = sz)
-axis(2, at=seq(0, 10, by=2), labels=seq(0, 10, by=2))
-mtext(2, text = 'cum. hosp. rate', line = sz, cex = sz2)
-legend('topleft', c('5-17 yr hosp.', '18-49 yr hosp.', 'pediatric deaths'), col = c('red', 'blue', 'forestgreen'), lwd = c(sz, sz, sz))
+plot(merge2$hosp_5.17, type = 'l', xlab = '', ylab = '', axes = F, lwd = sz,  cex.lab = sz, col = 'red', ylim = c(0, 26))
+lines(merge2$hosp_18.49, type = 'l', col = 'blue', lwd = sz)
+axis(2, at=seq(0, 26, by=10), labels=seq(0, 26, by=10))
+mtext(2, text = 'rate', line = sz3, cex = sz2)
+legend('topleft', c('5-17 yr hosp. rate', '18-49 yr hosp. rate', 'pediatric deaths'), col = c('red', 'blue', 'forestgreen'), lwd = c(sz, sz, sz, sz), title = 'Cumulative lab-confirmed:')
 
 # death panel
-par(mar=lastmargin) 
-plot(cdc_lm$pi_only, type = 'l', xlab = '', ylab = '', axes = F, lwd = 2, ylim = c(400, 1400), cex.lab = sz)
+par(mar=margin) 
+plot(merge2$pi_only, type = 'l', xlab = '', ylab = '', axes = F, lwd = 2, ylim = c(400, 1400), cex.lab = sz)
+abline(v = which(merge2$uqid=='201040'), lwd = sz2, col = 'black')
+abline(h = 400, lwd = sz2, col = 'black')
 axis(2, at = seq(400, 1400, by = 500), labels = seq(400, 1400, by = 500))
-mtext(2, text = 'P&I deaths (#)', line = 2, cex = sz2)
-axis(1, at = wkticks, labels = wklabs, las = 2, cex = sz)
+mtext(2, text = 'deaths', line = sz3, cex = sz2)
+legend('topleft', 'P&I-associated', col = 'black', lwd = sz)
+
+# benchmark panel
+par(mar=margin)
+mp <- barplot(benchmark$beta_norm0, xlab = '', ylab = '', axes = F, col = colorvec, cex.lab = sz, width=diff(wkticks), ylim = c(-1.5, 1.5))
+abline(v = (mp[13]+mp[12])/2, lwd = sz2, col = 'black') # force line bw 2008-09 and 2010-11 seasons
+abline(h = 0, lwd = sz2, col = 'black')
+axis(2, at = seq(-1, 1, by = 1), labels = seq(-1, 1, by = 1))
+mtext(2, text = "benchmark", line = sz3, cex = sz2)
+legend('topleft', c('Mild', 'Moderate', 'Severe'), col = c('blue', 'yellow', 'red'), lwd = c(sz, sz, sz))
+
+# x-axis
+axis(1, at = mp, labels = seasonlabs, las = 2, cex.axis = sz2)
 dev.off()
+
