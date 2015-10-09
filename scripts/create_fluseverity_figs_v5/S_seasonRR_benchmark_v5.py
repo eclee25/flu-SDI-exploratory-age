09e9f620aa4ed58547f943abed90f77a746d4b44
Elizabeth Lee <eclee25@gmail.com>
2015-10-09 12:13:58 -0400

    updated Fig S5
    S_seasonRR_benchmark
    - rm lines, add colors, save p-values, change Mild label position

--- scripts/create_fluseverity_figs_v5/S_seasonRR_benchmark_v5.py
+++ scripts/create_fluseverity_figs_v5/S_seasonRR_benchmark_v5.py
@@ -6,6 +6,7 @@
 ###Date: 1/20/15
 ###Function: relative risk of adult ILI to child ILI visits for the entire season vs. CDC benchmark index, mean Thanksgiving-based early zOR metric vs. CDC benchmark index. 
 # 7/20/15: update beta
+# 10/8/15: rm lines, color points, add p-values
 
 ###Import data: /home/elee/Dropbox/Elizabeth_Bansal_Lab/CDC_Source/Import_Data/cdc_severity_index.csv, SQL_export/OR_allweeks_outpatient.csv, SQL_export/totalpop_age.csv, My_Bansal_Lab/Clean_Data_for_Import/ThanksgivingWeekData_cl.csv
 
@@ -90,41 +91,41 @@ benchmark = [d_benchmark[s] for s in ps]
 fluSeason_RR = [entireSeasonRR(d_ageILIadj_season, d_pop, s) for s in ps]
 nonfluSeason_RR = [nonfluSeasonRR(d_ageILIadj_season, d_pop, s) for s in ps]
 tightfluSeason_RR = [tightSeasonRR(d_ageILIadj_season, d_pop, s) for s in ps]
+vals = zip(benchmark, fluSeason_RR, nonfluSeason_RR, tightfluSeason_RR)
+d_plotData = dict(zip(ps, vals))
+d_plotCol = fxn.gp_CDCclassif_ix
 
-# grab beta threshold values
-mildThresh, sevThresh = fxn.return_benchmark_thresholds(d_benchmark, d_qual_classif)
-
-# updated 7/20/15
-print 'entire flu season (40 to 20) corr coef', scipy.stats.pearsonr(benchmark, fluSeason_RR) # 0.789
-print 'non flu season corr coef', scipy.stats.pearsonr(benchmark, nonfluSeason_RR) # 0.217
-print 'tight flu season (50 to 12) corr coef', scipy.stats.pearsonr(benchmark, tightfluSeason_RR) # 0.825
+# updated 10/8/15
+print 'entire flu season (40 to 20) corr coef', scipy.stats.pearsonr(benchmark, fluSeason_RR) # R = 0.789, p-value = 0.020
+print 'non flu season corr coef', scipy.stats.pearsonr(benchmark, nonfluSeason_RR) # R = 0.217, p-value = 0.606
+print 'tight flu season (50 to 12) corr coef', scipy.stats.pearsonr(benchmark, tightfluSeason_RR) # R = 0.825, p-value = 0.012
 
 
 # draw plots
-fig1 = plt.figure()
-ax1 = fig1.add_subplot(1,1,1)
-# flu season RR vs. benchmark index
-ax1.plot(benchmark, fluSeason_RR, marker = 'o', color = 'black', linestyle = 'None')
-ax1.vlines([mildThresh, sevThresh], -20, 20, colors='k', linestyles='solid')
-ax1.annotate('Mild', xy=(-1.4,0.5), fontsize=fssml)
-ax1.annotate('Severe', xy=(1.1,0.5), fontsize=fssml)
-for s, x, y in zip(sl, benchmark, fluSeason_RR):
-	ax1.annotate(s, xy=(x,y), xytext=(-10,5), textcoords='offset points', fontsize=fssml)
-ax1.set_ylabel('Flu Season RR (R=0.79)', fontsize=fs) 
-ax1.set_xlabel(fxn.gp_benchmark, fontsize=fs)
-ax1.tick_params(axis='both', labelsize=fssml)
-ax1.set_xlim([-1.5,1.5])
-ax1.set_ylim([0,0.6])
-plt.savefig('/home/elee/Dropbox (Bansal Lab)/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/Submission_Materials/BMCMedicine/Submission2/SIFigures/seasonRR_benchmark.png', transparent=False, bbox_inches='tight', pad_inches=0)
-plt.close()
-# plt.show()
+# fig1 = plt.figure()
+# ax1 = fig1.add_subplot(1,1,1)
+# # flu season RR vs. benchmark index
+# for key in d_plotCol:
+# 	ax1.plot([d_plotData[k][0] for k in d_plotCol[key]], [d_plotData[k][1] for k in d_plotCol[key]], marker = 'o', color = key, linestyle = 'None')
+# ax1.annotate('Mild', xy=(-1.4,0.1), fontsize=fssml)
+# ax1.annotate('Severe', xy=(1.1,0.5), fontsize=fssml)
+# for s, x, y in zip(sl, benchmark, fluSeason_RR):
+# 	ax1.annotate(s, xy=(x,y), xytext=(-10,5), textcoords='offset points', fontsize=fssml)
+# ax1.set_ylabel('Flu Season RR (R=0.79)', fontsize=fs) 
+# ax1.set_xlabel(fxn.gp_benchmark, fontsize=fs)
+# ax1.tick_params(axis='both', labelsize=fssml)
+# ax1.set_xlim([-1.5,1.5])
+# ax1.set_ylim([0,0.6])
+# plt.savefig('/home/elee/Dropbox (Bansal Lab)/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/Submission_Materials/BMCMedicine/Submission3_ID/SIFigures/seasonRR_benchmark.png', transparent=False, bbox_inches='tight', pad_inches=0)
+# plt.close()
+# # plt.show()
 
 fig2 = plt.figure()
 ax2 = fig2.add_subplot(1,1,1)
 # nonflu season vs. benchmark index
-ax2.plot(benchmark, nonfluSeason_RR, marker = 'o', color = 'black', linestyle = 'None')
-ax2.vlines([mildThresh, sevThresh], -20, 20, colors='k', linestyles='solid')
-ax2.annotate('Mild', xy=(-1.4,0.5), fontsize=fssml)
+for key in d_plotCol:
+	ax2.plot([d_plotData[k][0] for k in d_plotCol[key]], [d_plotData[k][2] for k in d_plotCol[key]], marker = 'o', color = key, linestyle = 'None')
+ax2.annotate('Mild', xy=(-1.4,0.1), fontsize=fssml)
 ax2.annotate('Severe', xy=(1.1,0.5), fontsize=fssml)
 for s, x, y in zip(sl, benchmark, nonfluSeason_RR):
 	ax2.annotate(s, xy=(x,y), xytext=(-10,5), textcoords='offset points', fontsize=fssml)
@@ -133,16 +134,16 @@ ax2.set_xlabel(fxn.gp_benchmark, fontsize=fs)
 ax2.tick_params(axis='both', labelsize=fssml)
 ax2.set_xlim([-1.5,1.5])
 ax2.set_ylim([0,0.6])
-plt.savefig('/home/elee/Dropbox (Bansal Lab)/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/Submission_Materials/BMCMedicine/Submission2/SIFigures/nonfluseasonRR_benchmark.png', transparent=False, bbox_inches='tight', pad_inches=0)
+plt.savefig('/home/elee/Dropbox (Bansal Lab)/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/Submission_Materials/BMCMedicine/Submission3_ID/SIFigures/nonfluseasonRR_benchmark.png', transparent=False, bbox_inches='tight', pad_inches=0)
 plt.close()
 
 
 fig3 = plt.figure()
 ax3 = fig3.add_subplot(1,1,1)
 # tight flu season RR vs. benchmark index
-ax3.plot(benchmark, tightfluSeason_RR, marker = 'o', color = 'black', linestyle = 'None')
-ax3.vlines([mildThresh, sevThresh], -20, 20, colors='k', linestyles='solid')
-ax3.annotate('Mild', xy=(-1.4,0.5), fontsize=fssml)
+for key in d_plotCol:
+	ax3.plot([d_plotData[k][0] for k in d_plotCol[key]], [d_plotData[k][3] for k in d_plotCol[key]], marker = 'o', color = key, linestyle = 'None')
+ax3.annotate('Mild', xy=(-1.4,0.1), fontsize=fssml)
 ax3.annotate('Severe', xy=(1.1,0.5), fontsize=fssml)
 for s, x, y in zip(sl, benchmark, tightfluSeason_RR):
 	ax3.annotate(s, xy=(x,y), xytext=(-10,5), textcoords='offset points', fontsize=fssml)
@@ -151,5 +152,5 @@ ax3.set_xlabel(fxn.gp_benchmark, fontsize=fs)
 ax3.tick_params(axis='both', labelsize=fssml)
 ax3.set_xlim([-1.5,1.5])
 ax3.set_ylim([0,0.6])
-plt.savefig('/home/elee/Dropbox (Bansal Lab)/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/Submission_Materials/BMCMedicine/Submission2/SIFigures/tightseasonRR_benchmark.png', transparent=False, bbox_inches='tight', pad_inches=0)
+plt.savefig('/home/elee/Dropbox (Bansal Lab)/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/Submission_Materials/BMCMedicine/Submission3_ID/SIFigures/tightseasonRR_benchmark.png', transparent=False, bbox_inches='tight', pad_inches=0)
 plt.close()
