e6cb1ce3058a719f139f840152b6e9ad4915cec3
Elizabeth Lee <eclee25@gmail.com>
2015-10-15 14:50:15 -0400

    permutation test, corrmx
    S_mechanism_regression
    - implemented permutation test for corr coef instead of asymptotic
    test in the base function cor.test
    - replotted figures for SM

--- scripts/create_fluseverity_figs_v5/S_mechanism_regression_v5.R
+++ scripts/create_fluseverity_figs_v5/S_mechanism_regression_v5.R
@@ -150,7 +150,7 @@ ps = 14
 margin = c(1, 4, 1, 4) + 0.1 # bottom, left, top, right
 omargin = c(1, 1, 1, 1)
 un = "px"
-setwd('/home/elee/Dropbox (Bansal Lab)/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/Submission_Materials/BMCMedicine/Submission2/SIFigures')
+setwd('/home/elee/Dropbox (Bansal Lab)/Elizabeth_Bansal_Lab/Manuscripts/Age_Severity/Submission_Materials/BMCMedicine/Submission3_ID/SIFigures')
 
 ###################################
 panel.hist <- function(x, ...)
@@ -189,12 +189,41 @@ cor.mtest <- function(mat, conf.level = 0.95){
   }
   return(list(p.mat, lowCI.mat, uppCI.mat))
 }
+
+permTest <- function(vec1, vec2){
+  n.perms <- 1000
+  dummycor <- rep(NA, n.perms)
+  R <- cor(vec1, vec2)
+  for (i in 1:n.perms){
+    vec1.perm <- sample(vec1, length(vec1))
+    dummycor[i] <- cor(vec1.perm, vec2)
+  }
+  # p-value in a permutation test is the proportion of permute-calculated R values larger than R of the original samples
+  # two-sided test
+  pval <- length(dummycor[abs(dummycor) > abs(R)])/n.perms
+  return(list(R, p.value = pval))
+}
+
+cor.permtest <- function(mat, conf.level = 0.95){
+  mat <- as.matrix(mat)
+  n <- ncol(mat)
+  p.mat <- matrix(NA, n, n)
+  diag(p.mat) <- 0
+  for(i in 1:(n-1)){
+    for(j in (i+1):n){
+      tmp <- permTest(mat[,i], mat[,j])
+      p.mat[i,j] <- p.mat[j,i] <- tmp$p.value
+    }
+  }
+  return(list(p.mat))
+}
+
 ###################################
 # viz of correlation matrix
 colindexes <- c(2:7, 10, 8, 9)
 nat_sub <- nat[,colindexes]
 cor.mx <- cor(nat_sub) # corr matrix
-cor.pval <- cor.mtest(nat_sub,0.95) # p-values of correlation coefficients
+cor.pval <- cor.permtest(nat_sub) # p-values of correlation coefficients
 
 par(mar = margin)
 png(filename="corrmx_pairs.png", units=un, width=w, height=h, pointsize=ps, bg = 'white')
@@ -219,158 +248,4 @@ png(filename="corrmx_significant.png", units=un, width=w, height=h, pointsize=ps
 corrplot(cor.mx, method='ellipse', type='lower', tl.col='black', diag=FALSE, p.mat=cor.pval[[1]], insig='pch', sig.level=0.05)
 dev.off() # saved 7/20/15 5:34 pm
 
-# 7/20/15: not in analysis
-# # RR charts (nothing is significant with the benchmark)
-# colindexes2 <- c(2, 3, 15, 11, 14, 13, 12, 8, 9)
-# nat_sub2 <- nat[,colindexes2]
-# cor.mx2 <- cor(nat_sub2)
-# cor.pval2 <- cor.mtest(nat_sub2, 0.95)
-# corrplot(cor.mx2, method='ellipse', type='lower', tl.col='black', diag=FALSE, p.mat=cor.pval2[[1]], insig='pch', sig.level=0.05, addCoef.col='black')
-
-###################################
-# ## nation level linear models ## 
-# # leaps example
-# leaps1 <- regsubsets(ix_noILI ~ H3_prop + OR_ca + OR_ce + OR_ct + precip + temp + ili_a + ili_c + ili_t + ili_e, data=nat, offset = ili_tot, nbest=10)
-# summary(leaps1)
-# plot(leaps1, scale='r2')
-
-# # for steps adjacent to "best" model (lowest AIC model)
-# # calculate delta AIC
-# calculateDeltaAIC <- function(AICtable){
-#   minAIC <- min(AICtable$AIC)
-#   AICtable$deltaAIC <- AICtable$AIC - minAIC
-#   return (AICtable)
-# }
-# # calculate Akaike weights (okay for nested models)
-# calculateAkaikeWts <- function(AICtable){
-#   if ('deltaAIC' %in% names(AICtable)){
-#     numerators <- exp(-AICtable$deltaAIC/2)
-#     denom <- sum(numerators)
-#     AICtable$AkaikeWts <- numerators/denom
-#   }
-#   else {
-#     AICtable <- calculateDeltaAIC(AICtable)
-#     numerators <- exp(-AICtable$deltaAIC/2)
-#     denom <- sum(numerators)
-#     AICtable$AkaikeWts <- numerators/denom
-#   }
-#   return (AICtable)
-# }
-
-
-## no offset (ILIs) ##
-Mnat3n <- lm(ix_noILI ~ H3_prop + precip + temp +  ili_a + ili_c + ili_t + ili_e, data=nat)
-summary(Mnat3n)
-# step3n <- stepAIC(Mnat3n, direction='both', k=log(dim(nat)[1])) # BIC
-step3n <- stepAIC(Mnat3n, direction='both')
-step3n$anova
-Mnat3nf <- lm(ix_noILI ~ H3_prop+ temp + ili_a + ili_e, data=nat)
-summary(Mnat3nf)
-
-test <- lm(ix_noILI ~ H3_prop, data=nat)
-summary(test)
-
-plot(nat$ili_e, nat$temp)
-plot(nat$ili_t, nat$temp)
-
-
-## no offset (ORs) ##
-Mnat2n <- lm(ix_noILI ~ H3_prop + precip + temp + OR_ac + OR_at + OR_ae, data=nat)
-summary(Mnat2n)
-step2n <- stepAIC(Mnat2n, direction='both')
-step2n$anova
-Mnat2nf <- lm(ix_noILI ~ H3_prop + temp + OR_ac, data=nat)
-summary(Mnat2nf)
-
-plot(nat$ili_a, nat$ix_noILI)
-cor(nat$ili_tot, nat$ix_noILI)
-# ###########################################################
-# ## OFFSETS ##
-# ## reg offset (all) ##
-# Mnat1 <- lm(ix_noILI ~ H3_prop + OR_ca + OR_ce + OR_ct + precip + temp + ili_a + ili_c + ili_t + ili_e, data=nat, offset = ili_tot)
-# summary(Mnat1)
-# step1 <- stepAIC(Mnat1, direction='both')
-# step1$anova
-# Mnat1f <- lm(ix_noILI ~ H3_prop + temp + ili_a, data=nat, offset = ili_tot)
-# summary(Mnat1f)
-# # data frame for age-specific ILI and OR AIC results
-# Mnat1_stepchanges <- c('none', '+OR_ce', '+ili_c', '+OR_ca', '+ili_t', '+ili_e', '+OR_ct', '+precip', '-temp', '-ili_a', '-H3_prop')
-# # Mnat1_AIC <- c(19.526, 20.319, 20.711, 20.728, 21.132, 21.276, 21.494, 21.520, 24.389, 24.771, 26.992)
-# # Mnat1_AICtab <- data.frame(steps = Mnat1_stepchanges, AIC = Mnat1_AIC)
-# # Mnat1_AICtab_wts <- calculateAkaikeWts(Mnat1_AICtab)
-# 
-# ## log offset (all) ##
-# Mnat1l <- lm(ix_noILI ~ H3_prop + OR_ca + OR_ce + OR_ct + precip + temp + ili_a + ili_c + ili_t + ili_e, data=nat, offset = log(ili_tot))
-# summary(Mnat1l)
-# step1l <- stepAIC(Mnat1l, direction='both')
-# step1l$anova
-# Mnat1lf <- lm(ix_noILI ~ H3_prop + temp + ili_a, data=nat, offset = log(ili_tot))
-# summary(Mnat1lf)
-# # data frame for age-specific ILI and OR AIC results
-# Mnat1l_stepchanges <- c('none', '+OR_ce', '+ili_c', '+OR_ca', '+ili_e', '+ili_t', '+OR_ct', '+precip', '-temp', '-ili_a', '-H3_prop')
-# # Mnat1l_AIC <- c(19.299, 20.321, 20.710, 20.772, 21.000, 21.072, 21.266, 21.297, 23.840, 23.878, 26.543)
-# # Mnat1l_AICtab <- data.frame(steps = Mnat1l_stepchanges, AIC = Mnat1l_AIC)
-# # Mnat1l_AICtab_wts <- calculateAkaikeWts(Mnat1l_AICtab)
-# 
-# ## reg offset (ORs) ##
-# Mnat2 <- lm(ix_noILI ~ H3_prop + OR_ca + OR_ce + OR_ct + precip + temp, data=nat, offset = ili_tot)
-# summary(Mnat2)
-# step2 <- stepAIC(Mnat2, direction='both')
-# step2$anova
-# Mnat2f <- lm(ix_noILI ~ H3_prop + OR_ca + temp, data=nat, offset = ili_tot)
-# summary(Mnat2f)
-# # data frame for OR AIC results
-# Mnat2_stepchanges <- c('none', '-OR_ca', '+OR_ce', '+OR_ct', '+precip', '-H3_prop', '-temp')
-# # Mnat2_AIC <- c(24.285, 24.771, 24.871, 25.673, 26.235, 34.199, 34.439)
-# # Mnat2_AICtab <- data.frame(steps = Mnat2_stepchanges, AIC = Mnat2_AIC)
-# # Mnat2_AICtab_wts <- calculateAkaikeWts(Mnat2_AICtab)
-# 
-# ## log offset (ORs) ##
-# Mnat2l <- lm(ix_noILI ~ H3_prop + OR_ca + OR_ce + OR_ct + precip + temp, data=nat, offset = log(ili_tot))
-# summary(Mnat2l)
-# step2l <- stepAIC(Mnat2l, direction='both')
-# step2l$anova
-# Mnat2lf <- lm(ix_noILI ~ H3_prop + OR_ca + temp, data=nat, offset = log(ili_tot))
-# summary(Mnat2lf)
-# # data frame for OR AIC results
-# Mnat2l_stepchanges <- c('none', '-OR_ca', '+OR_ce', '+OR_ct', '+precip', '-H3_prop', '-temp')
-# # Mnat2l_AIC <- c(23.404, 23.878, 24.104, 24.813, 25.383, 33.196, 33.232)
-# # Mnat2l_AICtab <- data.frame(steps = Mnat2l_stepchanges, AIC = Mnat2l_AIC)
-# # Mnat2l_AICtab_wts <- calculateAkaikeWts(Mnat2l_AICtab)
-#
-# ## log offset (ILIs) ##
-# Mnat3l <- lm(ix_noILI ~ H3_prop + precip + temp +  ili_a + ili_c + ili_t + ili_e, data=nat, offset = log(ili_tot))
-# summary(Mnat3l)
-# step3l <- stepAIC(Mnat3l, direction='both')
-# step3l$anova
-# Mnat3lf <- lm(ix_noILI ~ H3_prop + temp + ili_a, data=nat, offset = log(ili_tot))
-# summary(Mnat3lf)
-# # dataframe for age-specific ILI AIC results (log ili_tot offset)
-# Mnat3l_stepchanges <- c('none', '+ili_c', '+ili_e', '+ili_t', '+precip', '-temp', '-ili_a', '-H3_prop')
-# # Mnat3l_AIC <- c(19.299, 20.710, 21.000, 21.072, 21.297, 23.840, 23.878, 26.543)
-# # Mnat3l_AICtab <- data.frame(steps = Mnat3l_stepchanges, AIC = Mnat3l_AIC)
-# # Mnat3l_AICtab_wts <- calculateAkaikeWts(Mnat3l_AICtab)
-#
-# ## reg offset (ILIs) ##
-# Mnat3 <- lm(ix_noILI ~ H3_prop + precip + temp +  ili_a + ili_c + ili_t + ili_e, data=nat, offset = ili_tot)
-# summary(Mnat3)
-# step3 <- stepAIC(Mnat3, direction='both')
-# step3$anova
-# Mnat3f <- lm(ix_noILI ~ H3_prop + temp + ili_a, data=nat, offset = ili_tot)
-# summary(Mnat3f)
-# # dataframe for age-specific ILI AIC results (non log ili_tot offset)
-# Mnat3_stepchanges <- c('none', '+ili_c', '+ili_t', '+ili_e', '+precip', '-temp', '-ili_a', '-H3_prop')
-# # Mnat3_AIC <- c(19.526, 20.711, 21.132, 21.276, 21.520, 24.389, 24.771, 26.992)
-# # Mnat3_AICtab <- data.frame(steps = Mnat3_stepchanges, AIC = Mnat3_AIC)
-# # Mnat3_AICtab_wts <- calculateAkaikeWts(Mnat3_AICtab)
-
-# ## EXTRAS ##
-# ###################################
-# ## state to region crosswalk ## 
-# setwd('/home/elee/Dropbox/Elizabeth_Bansal_Lab/SDI_Data/explore/Py_export')
-# crosswalk <- read.csv('stateabbr_regnum_crosswalk.csv', header=T, colClasses = 'character')
-# ###################################
-# ## vaccine coverage by state ##
-# setwd('/home/elee/Dropbox/Elizabeth_Bansal_Lab/Vaccines/Vax_Coverage')
-# coverage <- read.csv('Vax_Coverage_WEAT_BRFSS.csv', header=T, colClasses = c('numeric', 'character', 'character', 'numeric', 'numeric', 'numeric', 'numeric'))
 
